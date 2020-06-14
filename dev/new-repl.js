5bf565db387c0f564cc92296ca53dc60c0016f0b
AndreMaz <andre.maz.90@gmail.com>
2020-06-14 13:25:33 +0100

    Init

---
+++ dev/new-repl.js
@@ -0,0 +1,24 @@
+const { ServiceBroker } = require('moleculer')
+
+const broker = new ServiceBroker({
+    replLocation: '../../../index'
+})
+
+broker.createService({
+	name: "greeter",
+	actions: {
+		hello () { return 'Hello' },
+
+		welcome: {
+			rest: "/welcome",
+			params: { name: "string" },
+			async handler(ctx) {
+				return `Welcome, ${ctx.params.name}`;
+			}
+		}
+	},
+	events: {},
+})
+
+broker.start()
+    .then(() => broker.repl())
