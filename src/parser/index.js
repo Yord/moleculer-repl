84d5ad92e1a767d354bebfba81e0b9898ac716c9
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-17 09:08:51 +0200

    Add requireOpts to shargs parser so required option fields cause errors

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,5 +1,5 @@
 const {fromArgs: fromArgsDefault, lexerSync, parserSync} = require('shargs')
-const {arrayOnRepeat, flagsAsBools, restrictToOnly} = require('shargs-parser')
+const {arrayOnRepeat, flagsAsBools, restrictToOnly, requireOpts} = require('shargs-parser')
 const {bestGuess} = require('./bestGuess')
 const {groupOptions} = require('./groupOptions')
 const {nestKeys} = require('./nestKeys')
@@ -13,7 +13,7 @@ const lexer = lexerSync({
 
 const parser = cmd => rawCommand => parserSync({
   toArgv,
-  opts: [restrictToOnly, bestGuess, groupOptions, arrayOnRepeat],
+  opts: [requireOpts, restrictToOnly, bestGuess, groupOptions, arrayOnRepeat],
   args: [flagsAsBools, nestKeys],
   fromArgs: fromArgs(rawCommand)
 })(cmd)(rawCommand)
